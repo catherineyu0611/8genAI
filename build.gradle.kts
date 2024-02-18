7e7f0e5e9ffedca15196b9223f080e91e999537f
catherineyu0611 <152639186+catherineyu0611@users.noreply.github.com>
2024-02-18 10:36:07 -0500

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,49 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Code sourced from https://github.com/Everyday-Programmer/Android-Camera-using-CameraX/tree/a8d962161b9adf2cd21a04f1fb70b0a653f9022d
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.firstapp.wheresmywater"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.firstapp.wheresmywater"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // CameraX dependencies
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+
 }
